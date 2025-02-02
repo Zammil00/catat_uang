adca0facc93e59584471894754bd02d6cdbcc7bc
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-02 22:10:55 +0700

    Update Category Page

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -11,7 +11,106 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Switch(
+                  value: true,
+                  onChanged: (value) => {},
+                  inactiveTrackColor: Colors.purple,
+                  inactiveThumbColor: Colors.deepPurple,
+                  activeColor: Colors.red,
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.upload,
+                  color: Colors.red,
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.upload,
+                  color: Colors.red,
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.upload,
+                  color: Colors.red,
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Icon(
+                  Icons.upload,
+                  color: Colors.red,
+                ),
+                title: Text("Sedekah"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
