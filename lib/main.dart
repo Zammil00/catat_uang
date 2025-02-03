c92f3d13096336b65f00fc03150fa4db69f80295
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-03 18:59:46 +0700

    Update Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,8 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Lottie.asset(
           'assets/animations/splash.json', // Ganti dengan path animasi Lottie kamu
-          width: 200,
-          height: 200,
+          width: 300,
+          height: 300,
           fit: BoxFit.contain,
         ),
       ),
