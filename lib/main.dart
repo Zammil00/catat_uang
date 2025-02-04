34b939757f1890cc2cfe3522fb566138e80a2eaa
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-04 15:44:38 +0700

    Update Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import './pages/main_page.dart';
 
@@ -35,7 +36,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => MainPage()),
@@ -48,11 +49,25 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
       body: Center(
-        child: Lottie.asset(
-          'assets/animations/splash.json', // Ganti dengan path animasi Lottie kamu
-          width: 300,
-          height: 300,
-          fit: BoxFit.contain,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/animations/splash.json', // Ganti dengan path animasi Lottie kamu
+              width: 350,
+              height: 500,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'CATAT UANG',
+              style: GoogleFonts.roboto(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 1.5,
+              ),
+            ),
+          ],
         ),
       ),
     );
